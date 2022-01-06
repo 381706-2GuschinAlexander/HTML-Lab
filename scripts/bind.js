26595f944b93d946193dc0590c3bc963aa0ebc2e
igush <igush@DESKTOP-3OTTB5S>
2022-01-06 15:14:07 +0300

    base content

---
+++ scripts/bind.js
@@ -0,0 +1,23 @@
+function bindImageSwap(left_button, rigth_button, image_element="none", image_path="none", global_counter, img_count){
+
+    function setImage(name){
+        image_element.src= image_path + name + ".png";
+    };
+
+    setImage(global_counter.toString())
+
+    global_counter = 0;
+    left_button.onclick = function(){
+        --global_counter;
+        if( global_counter < 0)
+            global_counter = img_count - 1;
+
+        setImage(global_counter.toString())
+    };
+
+    rigth_button.onclick = function(){
+        global_counter = (global_counter + 1) % img_count;
+
+        setImage(global_counter.toString())
+    };
+}
